489137201962f3d0e43a3858c9292d1cb70cf1e4
Antoine Le Tellier <antoine.letellier90@gmail.com>
2026-08-26 16:30:23 +0200

    hello.c

--- test/hello.c
+++ test/hello.c
@@ -1,7 +1,14 @@
-int a[2] = {1, 2};
+#include <stdio.h>
 
-enum e { OUI = sizeof(a) };
+struct a {
+	int a;
+	int : 1;
+	int n;
+};
+
+enum s { OUI = sizeof(struct a) };
 
 int main(void) {
+	printf("%i\n", OUI);
 	return 0;
 }
