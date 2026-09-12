eff6944881146e64f87f2c7876890099573a675a
Antoine Le Tellier <antoine.letellier90@gmail.com>
2026-09-13 01:23:06 +0200

    renaming

--- crates/cc1/src/codegen/llvm/mod.rs
+++ crates/cc1/src/codegen/llvm/mod.rs
@@ -1,11 +1,11 @@
-pub mod binding;
 pub mod builder;
+pub mod name;
 pub mod operator;
+pub mod symbol;
 pub mod types;
-pub mod value;
 
-pub use binding::LlvmSymbol;
 pub use builder::Builder;
+pub use name::LlvmName;
 pub use operator::LlvmOperator;
+pub use symbol::LlvmSymbol;
 pub use types::LlvmType;
-pub use value::LlvmName;
